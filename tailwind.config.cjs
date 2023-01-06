f8ec33c46e307eed6332c31f0c11b11a85e2bc19
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2023-01-06 12:30:56 +0100

    Initial commit!

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+			  poppins: "Poppins",
+			  sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+			},
+		  },
+	},
+	plugins: [],
+}
