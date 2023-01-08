6c0dbbe77d15a5fea765accce1e472d1a05bf1c6
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2023-01-08 17:10:43 +0100

    Slight design changes

--- src/components/JobOpening.tsx
+++ src/components/JobOpening.tsx
@@ -4,12 +4,12 @@ export default function JobOpening(props: any) {
 
     return <div className="w-[340px] h-36 bg-slate-100 py-2 px-5 rounded-lg shadow-md">
         <div className="h-16">
-            <div className="font-bold text-lg mt-5 text-slate-800 leading-5">{jobOpening.title}</div>
-            <div className="font-semibold text-slate-700 mb-5">{jobOpening.location.name}</div>
+            <div className="font-bold text-lg mt-5 text-slate-900 leading-5">{jobOpening.title}</div>
+            <div className="font-semibold text-slate-500 mb-5">{jobOpening.location.name}</div>
         </div>
 
         <div className="justify-between gap-2 flex-wrap flex items-center">
-            <span className="">{"Updated at " + updateDate.toLocaleDateString("en-US")}</span>
+            <span className="text-slate-800">{"Updated at " + updateDate.toLocaleDateString("en-US")}</span>
             <a href={"https://boards.greenhouse.io/hypixelstudios/jobs/" + jobOpening.id} className="bg-blue-700 hover:bg-blue-800 py-2 px-4 rounded-md text-blue-50 font-medium">Apply</a>
         </div>
     </div>
