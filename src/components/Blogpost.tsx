fcd386d7109d4724246841ef74f76ebff363ea0e
Hugo Vallejo <hugo.vallejo@noxcrew.com>
2023-10-31 23:39:41 +0100

    Dark mode tweaks!

--- src/components/Blogpost.tsx
+++ src/components/Blogpost.tsx
@@ -12,17 +12,17 @@ export default function Blogpost(props: any) {
             "/" +
             post.slug) : "#"
     } className="h-full md:h-[190px] md:w-full w-[365px] sm:w-[380px]">
-        <div className="flex flex-col md:flex-row rounded-2xl shadow-md hover:shadow-lg">
+        <div className="flex flex-col md:flex-row rounded-2xl shadow-md hover:shadow-lg dark:hover:shadow-slate-900/80">
             <img src={post ? ("https://cdn.hytale.com/variants/blog_thumb_" +
                 post.coverImage.s3Key) : "tv-glitch.webp"} alt={post ? "Cover for a blogpost entry." : "Cover for unloaded blogpost."} className="md:max-w-[334px] rounded-tl-2xl md:rounded-bl-2xl rounded-bl-none rounded-tr-2xl md:rounded-tr-none overflow-x-clip" />
 
             <div className="bg-slate-100 dark:bg-slate-800 py-5 px-8 md:rounded-tr-2xl rounded-br-2xl rounded-bl-2xl md:rounded-bl-none h-[265px] md:h-auto w-full">
                 <div className="max-h-full md:max-h-[130px] overflow-y-hidden">
-                    {post ? <div className="font-bold text-base md:text-lg text-slate-900 dark:text-slate-200 leading-5">{post.title}</div> :
+                    {post ? <div className="font-bold text-base md:text-lg text-slate-900 dark:text-slate-300 leading-5">{post.title}</div> :
                         <div className="border-t-[24px] animate-pulse rounded-md border-slate-500 lg:w-[290px] w-[260px] mb-1" />}
-                    {post ? <div className="pb-4 md:pb-2 dark:text-slate-400 text-slate-500 text-sm font-semibold">{postDate!.toLocaleDateString("en-US") + " by " + post.author} </div>
+                    {post ? <div className="pb-4 md:pb-2 text-slate-500 text-sm font-semibold">{postDate!.toLocaleDateString("en-US") + " by " + post.author} </div>
                         : <div className="border-t-[12px] animate-pulse rounded-md border-slate-300 w-[160px] mb-4" />}
-                    {post ? <div className="pb-5 dark:text-slate-300 text-slate-800 text-sm md:text-base font-medium">{post.bodyExcerpt} </div> :
+                    {post ? <div className="pb-5 dark:text-slate-400 text-slate-800 text-sm md:text-base font-medium">{post.bodyExcerpt} </div> :
                         <div><div className="border-t-[16px] animate-pulse rounded-md border-slate-400 w-full mb-2" />
                             <div className="border-t-[16px] animate-pulse rounded-md border-slate-400 w-4/5" /></div>}
                 </div>
--- src/components/EntryCard.tsx
+++ src/components/EntryCard.tsx
@@ -9,9 +9,9 @@ export default function EntryCard(props: any) {
           <div className="bg-slate-100 dark:bg-slate-800 md:py-10 py-5 px-8 rounded-b-2xl w-full relative shadow-md">
                <div>
                     <div className="h-[290px] md:h-[230px]">
-                         <div className="font-bold text-lg md:text-xl text-slate-900 dark:text-slate-200">{entry.title}</div>
-                         <div className="pb-5 text-slate-500 dark:text-slate-400 text-sm md:text-base font-semibold">{"Published on " + new Date(entry.date).toLocaleDateString("en-US")} </div>
-                         <div className="pb-5 text-slate-800 dark:text-slate-300 text-sm md:text-base font-medium" dangerouslySetInnerHTML={{ __html: entry.body }} />
+                         <div className="font-bold text-lg md:text-xl text-slate-900 dark:text-slate-300">{entry.title}</div>
+                         <div className="pb-5 text-slate-500 dark:text-slate-500 text-sm md:text-base font-semibold">{"Published on " + new Date(entry.date).toLocaleDateString("en-US")} </div>
+                         <div className="pb-5 text-slate-800 dark:text-slate-400 text-sm md:text-base font-medium" dangerouslySetInnerHTML={{ __html: entry.body }} />
                     </div>
 
                     <a href={entry.url} className="text-blue-700 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 font-semibold ">See more →</a>
